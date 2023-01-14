8c32e6ebd2cd8c9d571f481d4a6fc84ed3070d4f
Janu <abbyjunior600@gmail.com>
2023-01-14 10:51:51 +0700

    make modal for login and banner

---
+++ components/Card/Banner.jsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import React from "react";
+import Banner1 from "../../public/banner/7b165985-d594-449f-acc5-5dc31339d87d.jpg.webp";
+import Banner2 from "../../public/banner/59e9ecd0-b91b-40d4-aef8-b1057be0_auto_x2.jpg";
+import Banner3 from "../../public/banner/b1d2ed1e-ef80-4d7a-869f-a0394f0629be.jpg";
+import Banner4 from "../../public/banner/ea701ee6-f36b-473d-b429-4d2a1da0713d.jpg";
+
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+
+function Banner() {
+  return (
+    <div className="w-full flex justify-center my-10">
+      <div className="absolute z-50 w-10/12 h-72 flex justify-between items-center">
+        <button className="p-2 rounded-full bg-white">
+          <IoIosArrowBack />
+        </button>
+        <button className="p-2 rounded-full bg-white">
+          <IoIosArrowForward />
+        </button>
+      </div>
+      <Image src={Banner1} layout="cover" className="rounded-2xl" />
+    </div>
+  );
+}
+
+export default Banner;
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,42 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import { HiOutlineSearch, HiShoppingCart } from "react-icons/hi";
+import Login from "./Modal/Login";
 
 function Header() {
+  const [loginModal, setLoginModal] = useState(false);
+
+  // if (loginModal) {
+  //   document.body.classList.add("active-modal");
+  // } else {
+  //   document.body.classList.remove("active-modal");
+  // }
+
   return (
-    <div className="sticky top-0 z-50 h-20 w-full px-10 flex items-center justify-between shadow-md">
-      {/* Left Content */}
-      <button className="text-3xl font-semibold text-green-500">
-        kokopedia
-      </button>
+    <>
+      <div className="sticky top-0 z-50 h-20 w-full px-10 flex items-center justify-between shadow-md">
+        {/* Left Content */}
+        <button className="text-3xl font-semibold text-green-500">
+          kokopedia
+        </button>
 
-      {/* Center Content */}
-      <form
-        type="submit"
-        className="w-[450px] flex items-center border rounded-lg text-sm"
-      >
-        <div className="w-full  px-3 py-1">
-          <input
-            type="text"
-            placeholder="Cari di Kokopedia"
-            className="w-full focus:outline-none"
-          />
+        {/* Center Content */}
+        <form
+          type="submit"
+          className="w-[450px] flex items-center border rounded-lg text-sm"
+        >
+          <div className="w-full  px-3 py-1">
+            <input
+              type="text"
+              placeholder="Cari di Kokopedia"
+              className="w-full bg-transparent focus:outline-none"
+            />
+          </div>
+          <button className="p-2 bg-gray-100 rounded-r-md text-lg hover:brightness-90">
+            <HiOutlineSearch className="text-gray-400 font-bold" />
+          </button>
+        </form>
+
+        {/* Right Content */}
+        <div className="flex items-center gap-4">
+          <button
+            // onClick={() => setLoginModal(!loginModal)}
+            className="py-2 px-3 rounded-lg text-lg hover:bg-gray-100 active:scale-90"
+          >
+            <HiShoppingCart className="text-gray-500 font-bold" />
+          </button>
+          <span className="h-6 border"></span>
+          <button
+            onClick={() => setLoginModal(!loginModal)}
+            className="px-4 py-1 font-semibold text-green-600 border-2 rounded-lg border-green-600 hover:bg-green-600 hover:text-white active:scale-90"
+          >
+            Masuk
+          </button>
         </div>
-        <button className="p-2 bg-gray-100 rounded-r-md text-lg hover:brightness-90">
-          <HiOutlineSearch className="text-gray-400 font-bold" />
-        </button>
-      </form>
-
-      {/* Right Content */}
-      <div className="flex items-center gap-4">
-        <button className="py-2 px-3 rounded-lg text-lg hover:bg-gray-100 active:scale-90">
-          <HiShoppingCart className="text-gray-500 font-bold" />
-        </button>
-        <span className="h-6 border"></span>
-        <button className="px-4 py-1 font-semibold text-green-600 border-2 rounded-lg border-green-600 hover:bg-green-600 hover:text-white active:scale-90">
-          Masuk
-        </button>
       </div>
-    </div>
+
+      {/* Login Modal */}
+      {loginModal && (
+        <div className="w-screen h-screen fixed top-0 left-0">
+          <div
+            onClick={() => setLoginModal(!loginModal)}
+            className="overlay"
+          ></div>
+          <div className="modal-login max-h-[556px] w-[420px] p-8">
+            <Login onModal={() => setLoginModal(!loginModal)} />
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
