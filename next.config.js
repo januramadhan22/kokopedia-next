6bb739dcb4b4c9ee51c528ae7f2600eb65ca37d3
Janu <abbyjunior600@gmail.com>
2023-01-15 13:52:34 +0700

    add list product and get product

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: "https://images.tokopedia.net",
-  },
+  images: ["https://fakestoreapi.com"],
 };
 module.exports = nextConfig;
